b82ecad667226f477f80d24ef63b0a1ccf2e7ccd
pm1975 <pm1975@windowslive.com>
2019-02-22 06:20:38 +0100

    Pierwszy program. Obliczanie zadania.

--- pierwszy/main.cpp
+++ pierwszy/main.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
+int uczniowie,cukierki,x,y;
+
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "Moja linia" << endl;
+    cout << "2019-02-21 18:46. macOS Majove. Wersja 10.14.3." << endl;
+
+    cout << "Ilu uczniow jest w Twojej klasie:";
+    cin >> uczniowie;
+
+    cout << "Ilu cukierkow kupila mama:";
+    cin >> cukierki;
+
+    x = cukierki/(uczniowie-1);
+    cout << "Cukierkow dla kazdego ucznia:" << x;
+
+    y = cukierki-x*(uczniowie -1);
+    cout << endl << "Dla Jasia na wieczor: " << y;
+
     return 0;
 }
